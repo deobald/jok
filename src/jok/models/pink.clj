d2b66d32b5bb9c0c4723d1d5f121c7aa5c5285fa
Steven Deobald <steven@c42.in>
2012-06-10 21:32:01 +0530

    queue is now a vector. finished callback plays the next song.

--- src/jok/models/pink.clj
+++ src/jok/models/pink.clj
@@ -2,18 +2,27 @@
   (:require [jok.music-player :as player]))
 
 (defn new-queue []
-  (clojure.lang.PersistentQueue/EMPTY))
+  [])
 
 (def queue (atom (new-queue)))
 
 (defn enqueue! [song]
   (swap! queue #(conj % song)))
 
+(defn dequeue! []
+  (swap! queue #(vec (rest %))))
+
 (defn current-song []
-  (peek @queue))
+  (first @queue))
 
 (defn play []
   (player/play (:file (current-song))))
 
+(defn play-next []
+  (dequeue!)
+  (player/play-song (:file (current-song))))
+
 (defn pause []
- (player/pause))
+  (player/pause))
+
+(player/on-finished play-next)
--- src/jok/music_player.clj
+++ src/jok/music_player.clj
@@ -1,19 +1,36 @@
 (ns jok.music-player
   (:require [jok.config :as config])
   (:import [uk.co.caprica.vlcj.player.headless DefaultHeadlessMediaPlayer]
-           [uk.co.caprica.vlcj.player MediaPlayerFactory]))
+           [uk.co.caprica.vlcj.player
+            MediaPlayerFactory
+            MediaPlayerEventAdapter]))
 
 (def player (.newHeadlessMediaPlayer (MediaPlayerFactory.)))
 
-(defn media-options []
+(defn- song? []
+  (.isPlayable player))
+
+(defn- media-options []
   (if (config/stream?)
     (into-array [":sout=#rtp{dst=230.0.0.1,port=5555,mux=ts}"])
     (make-array String 0)))
 
+(defn unpause []
+  (.play player))
+
+(defn play-song [song]
+  (.playMedia player song (media-options)))
+
 (defn play [song]
-  (if (.isPlayable player)
-    (.play player)
-    (.playMedia player song (media-options))))
+  (if (song?)
+    (unpause)
+    (play-song song)))
 
 (defn pause []
   (.pause player))
+
+(defn on-finished [callback]
+  (.addMediaPlayerEventListener player
+                                (proxy [MediaPlayerEventAdapter] []
+                                  (finished [player]
+                                    (callback)))))
