d584cbbe539ced464c02452e8338aa06b6ac6d86
Steven Deobald <steven@c42.in>
2013-02-06 09:06:11 +0530

    [sd] removed the local vlcj; they put it in maven central. got libvlc working on mac with a :jvm-opts hack.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ office jukebox
 # install [leiningen](https://github.com/technomancy/leiningen)
 ## install java
 ## install gpg
-## install maven
-## kill the local cache: `rm -rf ~/.m2/repository/local`
 # `lein deps`
+# `lein ring server`
+## sanity-check "-Djna.library.path" in :jvm-opts of project.clj
 
--- project.clj
+++ project.clj
@@ -1,36 +1,19 @@
 (defproject
   jukebox "1.0.0-SNAPSHOT"
   :description "Jokbox"
-  :repositories {"project" "file:repo"}
+
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.jaudiotagger/jaudiotagger "2.0.1"]
                  [compojure "1.1.0"] 
                  [hiccup "1.0.0"]
                  [ring/ring-jetty-adapter "1.1.0"]
-                 [local/vlcj "2.1.0"]
-                 [net.java.dev.jna/jna "3.4.0"] ;; mvn does not resolve this dependency
-                 ;; [org.clojars.ghoseb/cron4j "2.2.1"]
-                 ;; [fleetdb "0.3.1"]
-                 ;; [corroborate "0.2.0"]
-                 ;; [ring-json-params "0.1.3"]
-                 ;; [ring-cors "0.0.2-SNAPSHOT"]
-                 ;; [clj-json "0.4.3"]
-                 ;; [org.mindrot/jbcrypt "0.3m"]
-                 ;; [jaad "0.8.3"]
-                 ;; [jl "1.0.1"]
-                 ;; [jogg "0.0.7"]
-                 ;; [jorbis "0.0.15"]
-                 ;; [mp3spi "1.9.5"]
-                 ;; [tritonus_share "0.3.6"]
-                 ;; [vorbisspi "1.0.3"]
-                 ;; [fs "1.1.2"]
-                 ]
-  :dev-dependencies [[lein-ring "0.7.1"]
-                     ;; [speclj "1.5.2"]
-                     ;; [speclj-growl "1.0.0-SNAPSHOT"]
-                     ]
+                 [uk.co.caprica/vlcj "2.2.0"]] 
+
+  :plugins [[lein-ring "0.8.2"]]
+
   :main jok.core
   :ring {:handler jok.core/app}
-  ;;  :test-path "spec/"
-  )
+
+  ;; TODO: this is a huge sadness. fix in a cross-platform way.
+  :jvm-opts ["-Djna.library.path=/Applications/VLC.app/Contents/MacOS/lib"])
