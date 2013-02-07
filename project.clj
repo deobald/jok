d2e17b9c15e7956339e84171c16487bfd6dced93
Steven Deobald <steven@c42.in>
2013-02-07 10:25:31 +0530

    [sd] added cljs. try on linux before converting coffeescript.

--- project.clj
+++ project.clj
@@ -2,18 +2,24 @@
   jukebox "1.0.0-SNAPSHOT"
   :description "Jokbox"
 
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.jaudiotagger/jaudiotagger "2.0.1"]
-                 [compojure "1.1.0"] 
+                 [compojure "1.1.0"]
                  [hiccup "1.0.0"]
                  [ring/ring-jetty-adapter "1.1.0"]
-                 [uk.co.caprica/vlcj "2.2.0"]] 
+                 [uk.co.caprica/vlcj "2.2.0"]]
 
-  :plugins [[lein-ring "0.8.2"]]
+  :plugins [[lein-ring "0.8.2"]
+            [lein-cljsbuild "0.3.0"]]
 
   :main jok.core
   :ring {:handler jok.core/app}
+
+  :cljsbuild {:builds
+              [{:source-paths ["src-cljs"]
+                :compiler {:optimizations :whitespace
+                           :pretty-print true}}]}
 
   ;; TODO: this is a huge sadness. fix in a cross-platform way.
   :jvm-opts ["-Djna.library.path=/Applications/VLC.app/Contents/MacOS/lib"])
---
+++ src-cljs/example.cljs
@@ -0,0 +1,3 @@
+(ns example)
+
+(js/alert "Hello from ClojureScript!")
